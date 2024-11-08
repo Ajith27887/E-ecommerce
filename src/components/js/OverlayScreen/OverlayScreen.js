8c0652d505b1ffa3b98d1d5a6bdb98049bf97817
Ajith27887 <ajithyogeshcoc@gmail.com>
2024-11-08 22:31:31 +0530

    Tag added

--- src/components/js/OverlayScreen/OverlayScreen.js
+++ src/components/js/OverlayScreen/OverlayScreen.js
@@ -1,11 +1,37 @@
+import { CiCirclePlus } from "react-icons/ci";
+import { IconContext } from "react-icons";
+import Detailpage from '../Detailpage/Detailpage'
+
 import './OverlayScreen.scss'
+import { useState } from "react";
+
 
 
 const OverlayScreen = (props = {}) => {
+    const {tags = '', name = '', rating = ''} = props,
+        [detail, setDetail] = useState(false),
+        handleHover = () => {
+            setDetail(true);
+        };
+    
     return(
-        <div className="overlay-screen">
-            <p>asd</p>
-        </div>
+        <IconContext.Provider
+        value={{ color: 'white', size: '75px', font: "bold" }}
+      >
+            <div className="overlay-screen">
+                <div className='badges-container'>
+                    <div>
+                        {tags.map(data => (
+                            <div className='badges'>{data}</div>
+                        ))}
+                    </div>
+                    <div className='plus'>
+                        <CiCirclePlus onClick={handleHover} style={{fontWeight: 'bolder'}}/>
+                    </div>
+                </div>
+            {detail && <Detailpage/>}
+            </div>
+        </IconContext.Provider>
     )
 }
 
--- src/components/js/RecipesListing/RecipesListing.js
+++ src/components/js/RecipesListing/RecipesListing.js
@@ -6,29 +6,23 @@ import '../RecipesListing/RecipesListing.scss'
 const RecipesListing = (props = {}) => {
     const {recipes = [], limit = '', total = '' } = props,
         [hover, setHover] = useState(false),
-        handlehover = () => {
-            setHover(true);
-            console.log('hi');
-            
+        handlehover = (id) => {
+            setHover(id);
         }
-    console.log(hover,'hover');
     
     return(
         <div className="recipes-listing">
             <div className="container-fluid">
                 <div className="row">
-                    {/* <div className="col-3">
-                        <p>test</p>
-                    </div> */}
                     <div className="col-12">
                         <div className="recipes-container">
                             <div className="row">
                                 {recipes.map(data => (
-                                    <div className="col-4">
+                                    <div key={data.id} className="col-4">
                                         <div className='racipes-image'>
                                             <div className='overlay-container'>
-                                                <img src={data.image} onMouseEnter={handlehover} className="mt-2 recipes" style={{width : '100%'}}/>
-                                                {hover && <OverlayScreen/>}
+                                                <img src={data.image} onMouseEnter={() => handlehover(data.id)} className="mt-2 recipes" style={{width : '100%'}}/>
+                                                {hover === data.id && <OverlayScreen {...data}/>}
                                             </div>
                                         </div>
                                     </div>    
