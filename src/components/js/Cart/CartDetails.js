e9a4e6d5c454ce61c24bbab56e69e5b412ea5d9f
Ajith27887 <ajithyogeshcoc@gmail.com>
2024-11-10 19:29:54 +0530

    cartlist added

--- src/components/js/Cart/CartDetails.js
+++ src/components/js/Cart/CartDetails.js
@@ -1,10 +1,19 @@
+import './CartDetails.scss'
+
 const CartDetails = (props = {}) =>{
         const {name = '',image = '', rating = '', reviewCount = '', prepTimeMinutes = '' , mealType = [], ingredients = '', cuisine = '', caloriesPerServing = ''} = props;
 
         return (
             <div className="mt-5">
-                <div >
-                    {name}
+                <div className="container CartList">
+                    <div className="row">
+                        <div className="col-3 px-0">
+                            <img style={{width : '100%'}} src={image}/>
+                        </div>
+                        <div className="col-9 p-3 bold">
+                            <h4 >{name}</h4>
+                        </div>
+                    </div>
                 </div>
             </div>
         )
